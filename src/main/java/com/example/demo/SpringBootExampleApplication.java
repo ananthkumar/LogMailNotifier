74a6a589badf6816186b23e2d7a9860e60fa7f4f
unknown <sananth949494@gmail.com>
2018-04-30 00:28:51 +0530

    Changes..

--- src/main/java/com/example/demo/SpringBootExampleApplication.java
+++ src/main/java/com/example/demo/SpringBootExampleApplication.java
@@ -10,32 +10,4 @@ public class SpringBootExampleApplication {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
 	
-	
-	
-
-	/*@PostConstruct
-	public void init() throws UnknownHostException{
-		SMTPAppender appender = new SMTPAppender();
-		System.out.println("++++++++++++>>>>>>>>>>======================<<<<<<<<<<++++++++==");
-		//<-- Email Configuration -->
-		appender.setBufferSize(512);
-		appender.setSMTPHost("smtp.gmail.com");
-		appender.setSMTPProtocol("smtps");
-		appender.setSMTPPort(465);
-		appender.setSMTPUsername("sananth949494@gmail.com");
-		appender.setSMTPPassword("nov15nov");
-		appender.setFrom("sananth949494@gmail.com");
-		appender.setTo("sananth949494@gmail.com");
-		appender.setSubject("Testing Log4j Mail Notification");
-		appender.setThreshold(Level.ERROR);
-		PatternLayout pl = new PatternLayout();
-		pl.setConversionPattern("%-7p %d [%t] %c %x - %m%n");
-		appender.setLayout(pl);
-		
-		appender.activateOptions();
-	    Logger.getRootLogger().addAppender(appender);
-		System.out.println("++++++++++++>>>>>>>>>>======================<<<<<<<<<<++++++++==");
-
-	}
-*/	
 }
--- src/main/java/com/example/demo/components/Log4jMailer.java
+++ src/main/java/com/example/demo/components/Log4jMailer.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.components;
 
 import java.net.UnknownHostException;
 
